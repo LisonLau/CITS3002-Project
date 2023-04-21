66f21267e1d7f6f546274bb3ad9aa78554b5877c
Lison Lau <allisonlhm@gmail.com>
2023-04-21 11:25:19 +0800

    implement QB create questions file

--- QuestionBank/QB.py
+++ QuestionBank/QB.py
@@ -1,11 +1,12 @@
 import csv
 import random
+import socket
 
 def getMCQ():
     MCquestions = []
-    with open("mcq.csv", "r") as csvlines:
-        lines = csv.reader(csvlines)
-        for line in lines:
+    with open("mcq.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
             MCquestions.append(line)
     return MCquestions
     
@@ -13,7 +14,7 @@ def getPCQ():
     PCquestions = []
     with open("pcq.csv", "r") as lines:
         for line in lines:
-            PCquestions.append(line.rstrip())
+            PCquestions.append([line.rstrip()])
     return PCquestions
 
 def getRandom():
@@ -21,4 +22,18 @@ def getRandom():
     random.shuffle(allQuestions)
     randomQuestions = allQuestions[:10]
     return randomQuestions
+
+def sendQuestions(student, password):
+    # filename format : student_password
+    filename = student + password + ".csv"
+    question_list = getRandom()
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(question_list)
+        
+        
+# def gradeMCQ():
+# def gradePCQ():
+    
+        
         
