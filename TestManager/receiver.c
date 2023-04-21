de202f8611aac5946b0b12f054d8d7bd5ba9afff
alicialauhx2002@gmail.com <alicialauhx>
2023-04-21 20:53:05 +0800

    separated receiver.c into functions

--- TestManager/receiver.c
+++ TestManager/receiver.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
+#include "receiver.h"
 
-#define BUFFERSIZE   1024
-#define USERNAME    "user1"
-#define PASSWORD    "pass1"
+void receive_file(char *filename) {
+    // Receive file contents and store into file
+    FILE *fp = fopen(filename, "wb");
+    
+    if (fp == NULL) {
+        perror("[-] File cannot be opened");
+        exit(EXIT_FAILURE);
+    }
 
-int main() {
-    char *host  = "127.0.0.1";  // host
-    int port    = 8080;         // port
-    int opt = 1;
-    int n;
-    char buffer[BUFFERSIZE];
-    int sockfd, newsockfd;
-    socklen_t addr_size;
-    struct sockaddr_in ser_addr, cli_addr;
+    while ((n = recv(newsockfd, buffer, BUFFERSIZE, 0)) > 0) {
+        fwrite(buffer, sizeof(char), n, fp);
+    }
+    printf("[+] File received successfully.\n");
+    fclose(fp);     // Close the file
 
-    // Create socket file descriptor
+    exit(EXIT_SUCCESS);
+}
+
+void receiver_setup() {
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("[-] Error in socket.");
         exit(EXIT_FAILURE);
@@ -27,18 +28,18 @@ int main() {
     // Clear the serv_addr struct
     memset((char *) &ser_addr, 0, sizeof(ser_addr));
 
-    // Attach socket to port
+    // Attach socket to PORT
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("[-] Error in attaching socket to port.");
+        perror("[-] Error in attaching socket to PORT.");
         exit(EXIT_FAILURE);
     }
-    printf("[+] Attaching socket to port successful.\n");
+    printf("[+] Attaching socket to PORT successful.\n");
 
     ser_addr.sin_family      = AF_INET;
-    ser_addr.sin_port        = htons(port);
+    ser_addr.sin_port          = htons(PORT);
     ser_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind socket to address and port
+    // Bind socket to address and PORT
     if (bind(sockfd, (struct sockaddr*)&ser_addr, sizeof(ser_addr)) < 0) {
         perror("[-] Error in binding.");
         exit(EXIT_FAILURE);
@@ -58,6 +59,10 @@ int main() {
         perror("[-] Error in accepting.");
         exit(EXIT_FAILURE);
     }
+}
+
+int main() {
+    receiver_setup();
 
     // Create custom filename
     char filename[100] = "";
@@ -65,19 +70,5 @@ int main() {
     strcat(filename, PASSWORD);
     strcat(filename, ".csv");
 
-    // Receive file contents and store into file
-    FILE *fp = fopen(filename, "wb");
-    
-    if (fp == NULL) {
-        perror("[-] File cannot be opened");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((n = recv(newsockfd, buffer, BUFFERSIZE, 0)) > 0) {
-        fwrite(buffer, sizeof(char), n, fp);
-    }
-    printf("[+] File received successfully.\n");
-    fclose(fp);     // Close the file
-
-    return EXIT_SUCCESS;
+    receive_file(filename);
 }
