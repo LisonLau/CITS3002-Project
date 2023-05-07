9b4ba1acdfa3e8b710d18249b82afb28909cac01
liannelong <126382316+liannelong@users.noreply.github.com>
2023-05-07 14:16:54 +0800

    Proper restore

--- QuestionBank/QB 2.py
+++
@@ -1,9 +0,0 @@
-# Student 1: Allison Lau   (23123849)
-# Student 2: Alicia Lau    (22955092)
-# Student 3: Li-Anne Long  (23192171)
-
-from QB_TM import *
-
-if __name__ == '__main__':
-    QB = QuestionBank()
-    QB.runQBserver()
--- TestManager/TM 2.c
+++
@@ -1,6 +0,0 @@
-#include "TM.h"
-
-int main(int argc, char const *argv[]) {
-    storeUsers();
-    runTMforWeb();
-}
