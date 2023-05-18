59f6e9f3c068a1af09d667295f822f1183072855
alicialauhx2002@gmail.com <alicialauhx>
2023-05-18 11:52:19 +0800

    restructured header file

--- tm/TM.h
+++ tm/TM.h
@@ -2,6 +2,8 @@
 // Student 2: Alicia Lau    (22955092)
 // Student 3: Li-Anne Long  (23192171)
 
+#include "structs.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,16 +18,8 @@
 #include <netdb.h>
 #include <signal.h>
 
-#define MAX_USERNAME_LENGTH 100
-#define MAX_PASSWORD_LENGTH 100
-#define MAX_QUESTION_LENGTH 500
-#define MAX_STUDENTS        100
-#define MAX_QUESTIONS       10
-#define MAX_OPTIONS         4
-#define MAX_OPTION_LENGTH   100
 #define MAX_CLIENTS         30
 
-#define BUFFERSIZE          1024
 #define HTTPSIZE            4096
 #define FILESIZE            8129
 #define HTMLSIZE            16384   
@@ -33,42 +27,6 @@
 #define SERVER_PORT         8080
 #define CLIENT_PORT         8888
 #define TEMP_IMG            "tempImg.png"
-
-typedef struct Questions {
-    char type[10];  
-    char question[MAX_QUESTION_LENGTH];
-    int  isMCQ;  // 1 means MCQ, 0 means PCQ
-    char options[MAX_OPTIONS][MAX_OPTION_LENGTH];
-    char finalStuAns[BUFFERSIZE];
-    int  numAttempts;
-    int  isCorrect;
-    int  isDone; // 1 means question is done, 0 means not done
-} Questions; 
-
-typedef struct Students {
-    char username[MAX_USERNAME_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
-    char filename[BUFFERSIZE];
-    char ipAddress[255];
-    int  hasQuesFile;
-    int  loggedIn;
-    int  grade;
-    Questions allocated[MAX_QUESTIONS];
-} Students;
-
-typedef struct Result {
-    int  isCorrect;
-    char studentAns[BUFFERSIZE];
-    char studentOutput[BUFFERSIZE];
-} Result;
-
-// Global variables
-Students    students[MAX_STUDENTS];
-int         currQuestion[MAX_STUDENTS]; // records current question the student is at
-int         numStudents;
-int         TMserver; // server socket file descriptor
-const char  *SERVER_HOST;
-const char  *CLIENT_HOST;
 
 // main.c
 extern void   clearMemory(int);
